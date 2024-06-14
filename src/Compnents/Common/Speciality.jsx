db436607e63bbc91b922d0899f4b39d683dfb727
avianurag721 <anurag.721721@gmail.com>
2024-06-14 12:39:33 +0530

    2nd poster

--- src/Compnents/Common/Speciality.jsx
+++ src/Compnents/Common/Speciality.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { FaArrowCircleRight } from "react-icons/fa";
+import cancer from "../../Images/section2 imges/CancerCare.jpg";
+import cardiac from "../../Images/section2 imges/cardiacCare.jpg";
+import gastro from "../../Images/section2 imges/gastroscience.jpg";
+import neuro from "../../Images/section2 imges/neuroscience.jpg";
+import ortho from "../../Images/section2 imges/orthopaedic.jpg";
+import renal from "../../Images/section2 imges/renalCare.jpg";
 
 const alphabets = [
   {
@@ -107,6 +113,38 @@ const alphabets = [
     nav: "",
   },
 ];
+const specialities = [
+  {
+    image: cancer,
+    speciality: "Cancer Care",
+    nav: "",
+  },
+  {
+    image: cardiac,
+    speciality: "Cardiac Care",
+    nav: "",
+  },
+  {
+    image: neuro,
+    speciality: "Neurosciences",
+    nav: "",
+  },
+  {
+    image: gastro,
+    speciality: "Gastrosciences",
+    nav: "",
+  },
+  {
+    image: ortho,
+    speciality: "Orthopaedics",
+    nav: "",
+  },
+  {
+    image: renal,
+    speciality: "Renal Care",
+    nav: "",
+  },
+];
 
 const Speciality = () => {
   return (
@@ -117,21 +155,35 @@ const Speciality = () => {
           An Ecosystem for Clinical Excellence
         </h2>
       </div>
-      <div className=" w-[33%]">
-        <div className=" flex justify-between py-4 border-b border-black w-full">
-          <img src="" alt="imgg" />
-          <div className="flex justify-between">
-            <p>Cardiac Care</p>
-            <p>Cardiac Care</p>
-          </div>
-        </div>
-        <p className=" cursor-pointer group w-full lg:w-[40%]  flex py-4 gap-3 items-center">
+
+      {/* central section  */}
+      <div className="  w-full lg:w-[33%] ">
+        {specialities.map((item) => {
+          return (
+            <div className=" group  flex justify-between py-2 border-b border-black w-full">
+              <img
+                className=" bg-transparent "
+                width={50}
+                src={item.image}
+                alt="imgg"
+              />
+              <div className=" group-hover:text-red-600  w-[70%] flex items-center justify-between">
+                <p> {item.speciality} </p>
+                <p className=" group-hover:translate-x-4 transition-all duration-300 pr-5 text-lg">
+                  <FaArrowCircleRight />
+                </p>
+              </div>
+            </div>
+          );
+        })}
+        <p className=" cursor-pointer group w-full lg:w-[40%]  flex pt-16 gap-3 items-center">
           <p>View All Specialities</p>
           <p className=" group-hover:translate-x-4 transition-all duration-300 text-red-600 text-lg">
             <FaArrowCircleRight />
           </p>
         </p>
       </div>
+
       {/* alphabets search division/  */}
       <div className=" w-[30%]  border-l border-2 p-4">
         <h2>Search By</h2>
@@ -157,8 +209,8 @@ const Speciality = () => {
             </p>
           ))}
         </div>
-        <p className=" cursor-pointer group w-full lg:w-[60%]  flex py-4 gap-3 items-center">
-          <p>View All Specialities</p>
+        <p className=" cursor-pointer group w-full lg:w-[60%]  flex  gap-3 items-center">
+          <p>View All Ailments</p>
           <p className=" group-hover:translate-x-4 transition-all duration-300 text-red-600 text-lg">
             <FaArrowCircleRight />
           </p>
--- src/Compnents/Home/Poster1.jsx
+++ src/Compnents/Home/Poster1.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // import posterImage from "../../cropped-fah-1.webp";
-import DottedImage from "../../Images/dottedwallpaper.webp"
-import FahPoster from "../../Images/fahhealthCare.jpeg"
-
+import DottedImage from "../../Images/dottedwallpaper.webp";
+import FahPoster from "../../Images/FahAnniversaryPoster.png";
 
 const Poster1 = () => {
   return (
-    <div style={{ backgroundImage: `url(${DottedImage})` }} className="bg-cover bg-center bg-red-600 flex justify-center  lg:flex-row flex-col items-center ">
+    <div
+      style={{ backgroundImage: `url(${DottedImage})` }}
+      className="bg-cover bg-center bg-red-600 flex justify-center  lg:flex-row flex-col items-center "
+    >
       {/* postereImage */}
       <div className="postereImage lg:w-[30%] w-[90%] flex justify-center items-center">
         <img
@@ -17,20 +19,24 @@ const Poster1 = () => {
       </div>
       {/* central text box  */}
       <div className="lg:w-[30%] w-[90%] ">
-        <p className=" text-slate-500 text-lg">
-          <span  className=" text-center">"</span>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
-          deleniti odit impedit. Sequi dicta eligendi molestias repudiandae eum
-          repellat, inventore, nobis sunt omnis temporibus excepturi natus!
-          Commodi provident blanditiis voluptatem.<span className="">" </span>
+        <p className=" text-white text-lg">
+          <span className=" text-center">"</span>
+          At FAH Superspeciality Hospital, our mission is to provide exceptional
+          healthcare with compassion and innovation. We are committed to
+          advancing medical science, improving patient outcomes, and ensuring
+          that every individual receives the highest standard of care. Together,
+          we strive to make a positive impact on the lives of our patients and
+          the community.<span className="">" </span>
         </p>
         <br />
-        <p>Dr. Ahmad Raza Khan</p>
-        <p>Managing Director</p>
+        <div className=" text-3xl font-serif text-red-600">
+          <p>Dr. Ahmed Raza Khan</p>
+          <p className=" text-lg">Managing Director</p>
+        </div>
       </div>
 
       {/* right image section  */}
-      <div className=" hidden lg:block lg:w-[30%] w-[90%] "> 
+      <div className=" hidden lg:block lg:w-[30%] w-[90%] ">
         <img src={FahPoster} alt="FahPoster" className=" p-4" width={400} />
       </div>
     </div>
