b759e16c91bfde1e18713a0490eefefa209cbcf9
avianurag721 <anurag.721721@gmail.com>
2024-06-15 13:36:09 +0530

    15/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Compnents/Home/HomePage";
 import Navbar from "./Compnents/Common/Navbar";
-import MobileNavbar from "./Compnents/Common/MobileNavbar";
 
 const App = () => {
   return (
